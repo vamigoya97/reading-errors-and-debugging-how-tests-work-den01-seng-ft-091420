91ced4bdf62f91485c24cc24ed5808ac6fc6f680
Victor Alberto Amigo <amigo.victor.97@gmail.com>
2020-07-03 00:40:31 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,3 +2,10 @@
 
 first_number = 10 
 second_number = 20
+
+sum = first_number + second_number
+
+difference = first_number - second_number
+product = first_number*second_number
+quotient = first_number/second_number
+
